0faa55b197e578de07fe032bc18def17be8f509d
1 <1>
2022-12-04 19:00:04 +0800

    2

--- main.go
+++ main.go
@@ -2,38 +2,37 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/qwe826344858/zoneslee/pkg/websocket"
+	"net/http"
 )
 
-func serverWS(pool *websocket.Pool,w http.ResponseWriter,r *http.Request){
-	fmt.Println("websocket endpoint reached")
-
-	conn,err := websocket.Upgrade(w,r)
-	if err!=nil{
-		fmt.Fprint(w,"%+v\n",err)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
 	}
 
 	client := &websocket.Client{
-		Conn : conn,
-		Pool : pool,
+		Conn: conn,
+		Pool: pool,
 	}
 
-	pool.Regsiter <- client
+	pool.Register <- client
 	client.Read()
 }
 
-func setupRoutes(){
-	pool:=websocket.NewPool()
+func setupRoutes() {
+	pool := websocket.NewPool()
 	go pool.Start()
 
-	http.HandleFunc("/ws",func(w http.ResponseWriter,r *http.Request){
-		serverWS(pool,w,r)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
 }
 
-func main(){
-	fmt.Println("多人聊天室开启")
+func main() {
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
-	http.ListenAndServe(":9000",nil)
+	http.ListenAndServe(":9000", nil)
 }
